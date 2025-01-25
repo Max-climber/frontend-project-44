61d3112c0bc9ca5f5526cebd6b576379d189c051
Max-climber <max.izrailev04@mail.ru>
2025-01-25 12:00:52 +0300

    update brain-canc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,32 +1,17 @@
-import sayHelloToUser from "../../src/cli.js";
-import readlineSync from "readline-sync";
 import { getRandomInt, getRandomOperation } from "../../src/index.js";
+import engine from "../../src/index.js";
 
-const main = () => {
-  const userName = sayHelloToUser();
+function getData() {
+  const randomNumber1 = getRandomInt(0, 100);
+  const randomNumber2 = getRandomInt(0, 101);
+  const randomOperation = getRandomOperation();
 
-  console.log("What is the result of the expression?");
+  const mathExpression = `${randomNumber1} ${randomOperation} ${randomNumber2}`;
 
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber1 = getRandomInt(0, 100);
-    const randomNumber2 = getRandomInt(0, 101);
-    const randomOperation = getRandomOperation();
+  const expectedAnswer = eval(mathExpression).toString();
+  return [mathExpression, expectedAnswer];
+}
 
-    const mathExpression = `${randomNumber1} ${randomOperation} ${randomNumber2}`;
-    console.log(`Question: ${mathExpression}`);
-    const userAnswer = readlineSync.question("Your answer: ");
-    const expectedAnswer = eval(mathExpression).toString();
+const description = `What is the result of the expression?`;
 
-    if (expectedAnswer === userAnswer) {
-      console.log("Correct!");
-    } else {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.\nLet's try again, ${userName}!`
-      );
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
-
-export default main;
+export default () => engine(description, getData);
--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,5 +1,6 @@
 import { getRandomInt } from "../../src/index.js";
 import engine from "../../src/index.js";
+
 function getData() {
   const isEven = (randomNumber) => randomNumber % 2 === 0;
   const randomNumber = getRandomInt(0, 100);
@@ -9,4 +10,5 @@ function getData() {
 }
 
 const description = `Answer "yes" if the number is even, otherwise answer "no".`;
+
 export default () => engine(description, getData);
